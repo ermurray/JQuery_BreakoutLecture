04cab1889d066f17b1807a4872ce15e0cb10233c
ermurray <eric@ermurray.ca>
2022-09-14 16:25:00 -0600

    jully11 chohort breakout

--- Public/scripts/client.js
+++ Public/scripts/client.js
@@ -1,41 +1,42 @@
+const createActivity = (data) => {
+   return (
+      $(
+        `<article>
+          <div>
+            <h1 class="newActivity">${data.activity}</h1>
+            <pre>${JSON.stringify(data, null, 2)}</pre>
+          </div>
+        </article>
+        `
+      )
+    );
+  };
 
-// 
-const createActivity = function(data) {
-  // const activityTitle = data.activity
-  return (
-    $(
-      `<article>
-        <div>
-          <h1 class="newActivity">${data.activity}</h1>
-          <pre>${JSON.stringify(data, null, 2)}</pre>
-        </div>
-      </article>`
-    )
-  );
-}
-
-// `http://www.boredapi.com/api/activity?type=${data}`
 
 $(document).ready(function(){
   $('.exampleBubble').submit( event => {
+    // console.log('event object =>>>>>>>>>>>',event);
     event.preventDefault();
     const data = $('#myInput').val()
 
-    $.ajax({
-      url: '/postStuff',
-      method: 'POST',
-      data: {data}
-    }).then((res) => console.log(res));
+    // $.ajax({
+    //   url: '/postStuff',
+    //   method: 'POST',
+    //   data: {data} 
+    // }).then(res => console.log(res))
 
     $.ajax({
       url: `http://www.boredapi.com/api/activity?type=${data}`,
       method: 'GET',
-    }).then((res) => {
-      const $activity = createActivity(res)
-      $('.myTarget').empty().append($activity)
+    }).then( res => {
+      const $activity = createActivity(res);
+      $('.myTarget').empty().append($activity);
+      $('.newActivity').click(function(){
+        $(this).toggleClass('changeRed');
+      })
     })
-    $('.myTarget').on('click', '.newActivity', function(){
-      $(this).toggleClass('changeRed')
-    } )
+
+
   })
 });
+
